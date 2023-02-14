5135a004a1451626c4afaea569bfebbd87981b4f
papadate <120418954+papadate@users.noreply.github.com>
2023-02-14 00:38:06 +0000

    Add files via upload

---
+++ Network_Security/Assessment & Feedback/EX1/component/own/byteList.java
@@ -0,0 +1,15 @@
+package own;
+
+public class byteList
+{
+    private byte[] iv;
+
+    public void setIv(byte[] iv)
+    {
+        this.iv = iv;
+    }
+
+    public byte[] getIv() {
+        return iv;
+    }
+}
